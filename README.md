192412ae0bc8287beb6e2313970c78b819266408
ryanchristo <12519942+ryanchristo@users.noreply.github.com>
2022-12-29 11:44:01 -0800

    update deploy, readme, and links

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 # schema
 
-linked data schemas for structured data
+standards-based linked data schemas
 
 ## Resources 
 
+- https://www.w3.org/
 - https://json-ld.org/
 - https://schema.org/
--- deploy.sh
+++ deploy.sh
@@ -12,6 +12,60 @@ mkdir -p "public"
 # add CNAME file
 echo 'schema.chora.io' >> public/CNAME
 
+# json template for all indexes
+index_json_template='{
+  "@context": "https://schema.org",
+  "@type": "BreadcrumbList",
+  "itemListElement": []
+}'
+
+# html redirect for all indexes
+index_html="<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv=\"refresh\" content=\"0; url='index.jsonld'\" />
+  </head>
+</html>"
+
+# set index json using template
+index_json="$index_json_template"
+
+# add contexts item to index json
+index_json=$(echo "$index_json" | jq -r '.itemListElement += [{
+  "@type": "ListItem",
+  "position": 1,
+  "item": {
+    "@id": "https://schema.chora.io/contexts/index.jsonld",
+    "name": "contexts"
+  }
+}]')
+
+# add examples item to index json
+index_json=$(echo "$index_json" | jq -r '.itemListElement += [{
+  "@type": "ListItem",
+  "position": 2,
+  "item": {
+    "@id": "https://schema.chora.io/examples/index.jsonld",
+    "name": "examples"
+  }
+}]')
+
+# add templates item to index json
+index_json=$(echo "$index_json" | jq -r '.itemListElement += [{
+  "@type": "ListItem",
+  "position": 3,
+  "item": {
+    "@id": "https://schema.chora.io/templates/index.jsonld",
+    "name": "templates"
+  }
+}]')
+
+# add site index json
+echo "$index_json" >> public/index.jsonld
+
+# add site index redirect
+echo "$index_html" >> public/index.html
+
 gen_public_files() {
 
   # copy jsonld files
@@ -20,59 +74,59 @@ gen_public_files() {
   # change directories
   cd public/"$1"
 
-  # file count
+  # index item count
   count=0
 
-  # file total
+  # index item total
   total=$(find . -type f | wc -l)
 
-  # add index
+  # set index json using template
+  index_json="$index_json_template"
+
+  # add index json
   for f in *; do
+
+    # increment count
     count=$((count+1))
-    if [ $count -eq 1 ]; then
-      echo "{" >> index.jsonld
-    fi
-    if [ $count -eq "$total" ]; then
-      echo "  \"${f%.jsonld}\": \"https://schema.chora.io/$1/$f\"" >> index.jsonld
-      echo "}" >> index.jsonld
-    else
-      echo "  \"${f%.jsonld}\": \"https://schema.chora.io/$1/$f\"," >> index.jsonld
-    fi
+
+    # set item id
+    id="https://schema.chora.io/$1/$f"
+
+    # set item name
+    name=$(echo "$f" | sed -e "s|.jsonld||")
+
+    # set jq arguments (argjson required for integer)
+    args="--argjson position $count --arg id $id --arg name $name"
+
+    # add index item to index json
+    index_json=$(echo "$index_json" | jq -r $args '.itemListElement += [{
+      "@type": "ListItem",
+      "position": $position,
+      "item": {
+        "@id": $id,
+        "name": $name
+      }
+    }]')
+
   done
 
+  # add index json
+  echo "$index_json" >> index.jsonld
+
   # add index redirect
-  echo "<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv=\"refresh\" content=\"0; url='index.jsonld'\" />
-  </head>
-</html>" >> index.html
+  echo "$index_html" >> index.html
 
   # change directories
   cd ../../
 }
 
+# generate public files
 gen_public_files "contexts"
 gen_public_files "examples"
 gen_public_files "templates"
 
 # change directories
 cd public
-
-# add index
-echo "{
-  \"contexts\": \"https://schema.chora.io/contexts/index.jsonld\",
-  \"examples\": \"https://schema.chora.io/examples/index.jsonld\",
-  \"templates\": \"https://schema.chora.io/templates/index.jsonld\"
-}" >> index.jsonld
-
-# add index redirect
-echo "<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv=\"refresh\" content=\"0; url='index.jsonld'\" />
-  </head>
-</html>" >> index.html
 
 # git init and commit
 git init
